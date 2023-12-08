3394b840be1894d7d337cb342c0668149d531c90
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2023-12-08 15:35:16 -0300

    first commit

---
+++ src/main/java/com/tcc/uffmaterias/UffMateriasApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.uffmaterias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UffMateriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UffMateriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/uffmaterias/UffMateriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.uffmaterias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UffMateriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
